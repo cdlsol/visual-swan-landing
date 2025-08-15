c707122948e58c2a704396b38a8d284a451142f1
cdlsol <carloslopez.cl265@gmail.com>
2025-08-14 21:39:41 -0600

    main branch

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="z-10 mt-8 animate-fade-in">
-        <img src="/visual_swan_logo.jpeg" alt="Visual Swan logo" className="mx-auto h-24 w-auto md:h-32" />
+        <img src="/vs_favicon.png" alt="Visual Swan logo" className="mx-auto h-24 w-auto md:h-32" />
       </div>
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
